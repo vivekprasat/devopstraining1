19ed97cec4ebb1a7c5bb86d805046f56a4e1d255
Vivek Prasath S <vsrajan2k@gmail.com>
2025-09-18 19:00:34 +0530

    Update remote_backend.tf

--- modules/remote_backend.tf
+++ modules/remote_backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform"
-    storage_account_name = "tfstateaccount"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
 
