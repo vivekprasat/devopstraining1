f9faa01929e2c2b72b226a93259adee31a44aee2
Vivek Prasath S <vsrajan2k@gmail.com>
2025-09-18 18:57:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,62 +3,65 @@ module "azurerm_resource_group" {
   resource_group_name     = var.group_name
   resource_group_location = var.location
 }
+
 module "azurerm_virtual_network" {
-  source         = "./modules/virtual _networks"
-  resourcegroup  = module.azurerm_resource_group.group_name
+  source         = "./modules/virtual_networks" # fixed path
+  resource_group = module.azurerm_resource_group.name
   location       = module.azurerm_resource_group.location
   vnet_name      = var.vnet_name
   address_prefix = var.address_prefix
   depends_on     = [module.azurerm_resource_group]
 }
+
 module "azurerm_subnet" {
   source                  = "./modules/subnets"
   subnet_address_prefixes = var.address_prefix
-  resource_group_name     = module.azurerm_resource_group.group_name
+  resource_group_name     = module.azurerm_resource_group.name
   name                    = var.sub_name
   vnet_name               = module.azurerm_virtual_network.vnet_name
-  depends_on              = [module.azurerm_virtual_network, module.azurerm_resource_group]
+  depends_on              = [module.azurerm_virtual_network]
 }
+
+module "azurerm_network_security_group" {
+  source              = "./modules/NSG"
+  name                = var.nsg_name
+  resource_group_name = module.azurerm_resource_group.name
+  location            = module.azurerm_resource_group.location
+  depends_on          = [module.azurerm_resource_group]
+}
+
 module "azurerm_public_ip" {
   source              = "./modules/PublicIPaddress"
-  resource_group_name = module.azurerm_resource_group.group_name
+  resource_group_name = module.azurerm_resource_group.name
   location            = module.azurerm_resource_group.location
-  azurerm_public_ip   = var.address_prefix
   name                = "net124"
   depends_on          = [module.azurerm_resource_group]
 }
+
 module "azurerm_network_interface" {
   source              = "./modules/virtualMachines1/NIC"
   nic_name            = var.nic_name
-  resource_group_name = module.azurerm_resource_group.group_name
+  resource_group_name = module.azurerm_resource_group.name
   subnet_id           = module.azurerm_subnet.subnet_id
   location            = module.azurerm_resource_group.location
   public_ip_id        = module.azurerm_public_ip.id
-  nsg_id              = module.azurerm_network_security_group.nsg_id
-  depends_on          = [module.azurerm_subnet]
-
+  nsg_id               = module.azurerm_network_security_group.nsg_id
+  depends_on          = [module.azurerm_subnet, module.azurerm_network_security_group]
 }
+
 module "virtualmachines" {
   source              = "./modules/virtualMachines1"
   vm_name             = var.vm_name
   location            = module.azurerm_resource_group.location
-  resource_group_name = module.azurerm_resource_group.group_name
+  resource_group_name = module.azurerm_resource_group.name
   subnet_id           = module.azurerm_subnet.subnet_id
   public_ip_id        = module.azurerm_public_ip.id
   admin_username      = "azureuser"
   admin_password      = "Revisu!123"
-  nic_name            = module.azurerm_network_interface.nic_name
+  nic_name             = module.azurerm_network_interface.nic_name
+  nic_id               = module.azurerm_network_interface.nic_id
+  nsg_id                = module.azurerm_network_security_group.nsg_id
   depends_on          = [module.azurerm_network_interface]
-  nic_id              = module.azurerm_network_interface.nic_id
-  nsg_id              = module.azurerm_network_security_group.nsg_id
-}
-module "azurerm_network_security_group" {
-  source              = "./modules/NSG"
-  name                = var.nsg_name
-  resource_group_name = module.azurerm_resource_group.group_name
-  location            = module.azurerm_resource_group.location
-  nic_name            = var.nic_name
-  depends_on          = [module.azurerm_resource_group]
 }
 
 terraform {
@@ -69,4 +72,3 @@ terraform {
     key                   = "terraform.tfstate"
   }
 }
-
