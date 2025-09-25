2f84fe206800162a41a532149182de6a968027ec
Vivek Prasath S <vsrajan2k@gmail.com>
2025-09-25 08:01:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "azurerm_resource_group" {
 
 module "azurerm_virtual_network" {
   source = "./modules/virtual _networks" # fixed folder name
-  resource_group_name = module.azurerm_resource_group.resource_group_name
-  location            = module.azurerm_resource_group.resource_group_location
+  resource_group_name = var.group_name
+  location            = var.location
   vnet_name           = var.vnet_name
   address_prefix      = var.address_prefix
   depends_on          = [module.azurerm_resource_group]
@@ -16,7 +16,7 @@ module "azurerm_virtual_network" {
 module "azurerm_subnet" {
   source                  = "./modules/subnets"
   subnet_address_prefixes = var.address_prefix
-  resource_group_name     = module.azurerm_resource_group.resource_group_name
+  resource_group_name     = var.group_name
   name                    = var.sub_name
   vnet_name               = module.azurerm_virtual_network.vnet_name
   depends_on              = [module.azurerm_virtual_network]
@@ -25,16 +25,16 @@ module "azurerm_subnet" {
 module "azurerm_network_security_group" {
   source              = "./modules/NSG"
   name                = var.nsg_name
-  resource_group_name = module.azurerm_resource_group.resource_group_name
-  location            = module.azurerm_resource_group.resource_group_location
+  resource_group_name = var.group_name
+  location            = var.location
   nic_name            = var.nic_name
   depends_on          = [module.azurerm_resource_group]
 }
 
 module "azurerm_public_ip" {
   source              = "./modules/PublicIPaddress"
-  resource_group_name = module.azurerm_resource_group.resource_group_name
-  location            = module.azurerm_resource_group.resource_group_location
+  resource_group_name = var.group_name
+  location            = var.location
   name      = "net124"
   sku                 = "Basic"
   allocation_method   = "Dynamic"
@@ -44,9 +44,9 @@ module "azurerm_public_ip" {
 module "azurerm_network_interface" {
   source              = "./modules/virtualMachines1/NIC"
   nic_name            = var.nic_name
-  resource_group_name = module.azurerm_resource_group.resource_group_name
+  resource_group_name = var.group_name
   subnet_id           = module.azurerm_subnet.subnet_id
-  location            = module.azurerm_resource_group.resource_group_location
+  location            = var.location
   public_ip_id        = module.azurerm_public_ip.id
   nsg_id              = module.azurerm_network_security_group.nsg_id
   depends_on          = [module.azurerm_subnet, module.azurerm_network_security_group]
@@ -55,8 +55,8 @@ module "azurerm_network_interface" {
 module "virtualmachines" {
   source              = "./modules/virtualMachines1"
   vm_name             = var.vm_name
-  location            = module.azurerm_resource_group.resource_group_location
-  resource_group_name = module.azurerm_resource_group.resource_group_name
+  location            = var.location
+  resource_group_name = var.group_name
   subnet_id           = module.azurerm_subnet.subnet_id
   public_ip_id        = module.azurerm_public_ip.id
   admin_username      = "azureuser"
